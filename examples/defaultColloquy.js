f8196745d760b4d60004c2e383e2f7213dd77181
David House <davidahouse@gmail.com>
2016-12-18 10:04:05 -0500

    wip commit

---
+++ examples/defaultColloquy.js
@@ -0,0 +1,7 @@
+module.exports = DefaultColloquy;
+
+function DefaultColloquy() {
+
+  var colloquy = TurboChatColloquy()
+
+}
--- examples/thing1.js
+++ examples/thing1.js
@@ -1,9 +1,33 @@
 var TurboChat = require('../lib/TurboChat.js');
-console.log('thing1 starting');
-var thing1cfg = {"name": "thing1",
-                 "knowledge": [],
-                 "attributes": [{"kind":"computer"}],
-                 "actions": ["scan"],
-                 "slack": {"token": process.env.THING1_SLACK_TOKEN}};
+
+console.log('--- setting configuration');
+var colloquies = {"thing1": "./examples/thing1.colloquy"};
+var thing1cfg = {"slack": {"token": process.env.THING1_SLACK_TOKEN},
+                 "slack-name": "secondary-hyperdrive",
+                 "colloquies": colloquies};
+
+console.log('--- creating turbochat object');
 var thing1 = new TurboChat(thing1cfg);
+
+thing1.prototype = {
+  openDoor: function() {
+    return "got the callback to open the door";
+  }
+}
+
+console.log('--- starting');
 thing1.start();
+
+// console.log('thing1 starting');
+// var thing1 = new TurboChat(thing1cfg);
+// thing1.start();
+
+// yaml = require('js-yaml');
+// fs = require('fs');
+//
+// try {
+//   var doc = yaml.safeLoad(fs.readFileSync('./examples/thing1.interface', 'utf8'));
+//   console.log(doc);
+// } catch (e) {
+//   console.log(e);
+// }
